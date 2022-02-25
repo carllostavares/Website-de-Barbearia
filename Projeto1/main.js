330d092aeaf96300cacfd406f10dc32a91ffd522
carllostavares <84608022+carllostavares@users.noreply.github.com>
2022-02-24 22:17:14 -0300

    Adicionando evento de click para fechar men

--- Projeto1/main.js
+++ Projeto1/main.js
@@ -1,8 +1,19 @@
+/* Abre e fecha o menu quando clicar no icone */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  })
+}
+
+/*Quando Clicar em um icone do menu, fehcar o menu*/
+
+const links = document.querySelectorAll('nav ul li a ')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
