25cdcee213f72e12f70fd633a752fbc8d48452df
carllostavares <84608022+carllostavares@users.noreply.github.com>
2022-03-05 00:21:45 -0300

    Adicionando divider 1 com informações sobre nós e a divider 2 com os Serviços. Juntamento com os  Cards e sua imagens. Adicionado também a mudança de cor no header na rolagem da tela.

--- Projeto1/main.js
+++ Projeto1/main.js
@@ -6,3 +6,25 @@ for (const element of toggle) {
     nav.classList.toggle('show')
   })
 }
+
+/* Quando clicar em um item do menu, esconder o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* MUDAR O HEADER DAGINA QUANDO DER SCROLL*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é meior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
