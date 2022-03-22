63abbbc15a45c1d989e9222a06fd5e1e26a61cc4
carllostavares <84608022+carllostavares@users.noreply.github.com>
2022-03-21 23:02:39 -0300

    Adicionando section de testemunhas com slide  e biblioterca Swiper

--- Projeto1/main.js
+++ Projeto1/main.js
@@ -28,3 +28,13 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/*----- Testimonials carousel slide swiper ----*/
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
